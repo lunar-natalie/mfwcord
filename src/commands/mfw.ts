88f0a164dad9120ae5cabc7f69fb47795b0e3eeb
Natalie Wiggins <islifepeachy@outlook.com>
2022-09-06 09:18:52 +0100

    feat: improve message algorithm

--- src/commands/mfw.ts
+++ src/commands/mfw.ts
@@ -4,18 +4,18 @@ import { Discord, Slash } from "discordx";
 @Discord()
 export class Mfw {
     private terms: Terms = {
-        secondary: [
-            "whenthe", "when", "the", "she", "he", "they", "it", "mfw",
-            "my face"
-        ],
-        primary: [
-            "me when", "mewhenthe"
-        ],
         prefixes: [
             "do you remember the",
             "do you know that feeling when the",
             "oh my god i forgor about the me the",
             "currently lmfao at that time when the"
+        ],
+        primary: [
+            "me when", "mewhenthe"
+        ],
+        secondary: [
+            "me", "when", "the", "I", "she", "he", "they", "it", "mfw",
+            "my face"
         ]
     };
 
@@ -28,8 +28,8 @@ export class Mfw {
     })
     show(interaction: CommandInteraction): void {
         interaction.reply(this.generate(this.terms, {
-            secondaryCount: Math.floor(400 * Math.random()),
-            primaryCount: Math.floor(5 * Math.random()),
+            primaryCount: Math.floor(20 * Math.random()),
+            secondaryCount: Math.floor(100 * Math.random()),
             maxLength: 2000
         }));
     }
@@ -44,24 +44,24 @@ export class Mfw {
         let result = "";
         let el: string;
 
-        el = getRandomElement(terms.prefixes) + " ";
+        el = getRandomElement(terms.prefixes);
         if (el.length < termAttribs.maxLength) {
             result = el;
         }
 
         for (let i = 0; i < termAttribs.primaryCount; ++i) {
-            el = getRandomElement(terms.primary) + " ";
+            el = " " + getRandomElement(terms.primary);
             for (let j = 0; j < termAttribs.secondaryCount; ++j) {
-                el += getRandomElement(terms.secondary) + " ";
+                el += " " + getRandomElement(terms.secondary);
                 if (result.length + el.length > termAttribs.maxLength) {
-                    return result.trim();
+                    return result;
                 }
             }
 
             result += el;
         }
 
-        return result.trim();
+        return result;
     }
 }
 
@@ -69,25 +69,25 @@ export class Mfw {
  * Term elements from which to generate a pseudo-random string.
  */
 interface Terms {
-    /** Elements in the main body. */
-    secondary: string[];
+    /** Elements to prefix the generated string with once. */
+    prefixes: string[];
 
     /** Elements to prioritize and use first. */
     primary: string[];
 
-    /** Elements to prefix the generated string with once. */
-    prefixes: string[];
+    /** Elements in the main body. */
+    secondary: string[];
 }
 
 /**
  * Attributes for usage of a Terms object.
  */
 interface TermAttributes {
-    /** Number of secondary terms to add to a generated string. */
-    secondaryCount: number;
-
     /** Number of primary terms to add to a generated string. */
     primaryCount: number;
+
+    /** Number of secondary terms to add to a generated string. */
+    secondaryCount: number;
 
     /** Maximum length of the string to generate. */
     maxLength: number;
